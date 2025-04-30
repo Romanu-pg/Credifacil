d79679153fc5f9e5ead85255b374fa9bbf059a77
Romanu-pg <56236826+Romanu-pg@users.noreply.github.com>
2025-04-29 22:22:40 -0500

    Quitar para ignorar el constats

---
+++ client/src/utils/constants.js
@@ -0,0 +1,14 @@
+export const HERO_URL = 'https://tech-ecommerce.s3.eu-west-2.amazonaws.com/images/hero2.png';
+
+export const ABOUT_IMG_URL = 'https://tech-ecommerce.s3.eu-west-2.amazonaws.com/images/hero1.png';
+
+export const API_URL = 'http://localhost:8080';
+
+
+export const CATEGORIES = [
+    'all', 'agricultura', 'industria Química', 'eficiencia Energética', 'energia Renovable', 'hogar y Comunidad', 'biomasa'
+];
+
+export const BRANDS = [
+    'All', 'GreenEarth', 'WindTech', 'EcoEnergy', 'PhoneTech'
+];
